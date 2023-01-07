409f2b983b411fe5508bf21feb0442e1d25d60fc
Ajith-Kumar0408 <ajithak0093@gmail.com>
2023-01-07 17:18:06 +0530

    end

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-var res=button.addEventListener("button");
+  var container=document.createElement("div");
+  container.setAttribute("class","container");
+  var row=document.createElement("div");
+    row.setAttribute("class","row");
+     container.append(row);
+
+    var res=fetch("https://anapioficeandfire.com/api/characters/583");
+    res.then((data)=>data.json()).then((data1)=>foo(data1));
+    function foo(data1){
+    row.innerHTML+= `Name :${data1.name}<br>
+    Gender : ${data1.gender}<br>
+    Title : ${data1.titles}<br>
+    Culture : ${data1.culture}<br>
+    Born : ${data1.born}`;
+    document.body.append(container);
+}
